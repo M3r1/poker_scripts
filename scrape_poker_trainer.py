396d597844ee357ebf3cb33a31a87fefa41a07fd
M3r1 <gili.mery@gmail.com>
2025-02-03 19:33:06 +0200

    asdf

--- scrape_poker_trainer.py
+++ scrape_poker_trainer.py
@@ -11,20 +11,23 @@ RAISE = "RAISE"
 NAN = "NaN"
 BTN = "BTN"
 LJ = "LJ"
+HJ = "HJ"
 BB = "BB"
+SB = "SB"
 
 ### SCENARIOS
 VS_RAISE = "vs raise"
 VS_3BET = "vs 3bet"
+VS_4BET = "vs 4bet"
 OPEN = "Open"
 
 #######################
 #######################
 #######################
-SCENARIO = VS_RAISE
-HERO = BB
-VILLAIN = BTN
-ACTION = CALL
+SCENARIO = VS_3BET
+HERO = LJ
+VILLAIN = BB
+ACTION = RAISE
 #######################
 #######################
 #######################
@@ -41,19 +44,52 @@ def get_all_elements(selenium_driver, find_elements_method, elements_identifier)
 def get_elements_by_text(all_elements, element_text):
     return [btn for btn in all_elements if btn.text==element_text]
 
-def get_combo_percentage(call_or_raise, combo_style):
+def get_combo_percentage(call_or_raise, combo_style, is_fraction):
     call_or_raise = get_call_or_raise_percentage_text(call_or_raise)
 
     all_attributes = combo_style.split(";")
     call_or_raise_percentage = next(attribute for attribute in all_attributes if call_or_raise in attribute)
     percentage = call_or_raise_percentage.split(":")[1].strip()
+
+    if is_fraction:
+        fraction = next(attribute for attribute in all_attributes if "--action-height" in attribute)
+        fraction = fraction.split(":")[1].strip()[0:-1]
+
+        if fraction == "NaN":
+            fraction = "0"
+        
+        percentage = float(percentage) * (float(fraction) / 100.0)
     
     return round((float(percentage) * 100))
 
-options = Options()
+def get_flopzilla_range_string(all_combo_elements, is_fraction):
+    all_combos_filtered = [combo for combo in all_combo_elements if combo.text != "BUILT-IN" and combo.text != "YOUR RANGES" and combo.text != ""] 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
-                          options=options)
+    flopzilla_range_string = ""
+    for combo in all_combos_filtered:
+        percentage = get_combo_percentage(ACTION, combo.get_attribute("style"), is_fraction)
+        if percentage != 0:
+            flopzilla_range_string = flopzilla_range_string + f"[{percentage}]{combo.text}[/{percentage}]" + ","
+
+    return flopzilla_range_string[:-1]
+
+def get_open_or_vs_raise_flopzilla_range_string(all_landing_page_game_elements, driver, scenario):
+    get_elements_by_text(all_landing_page_game_elements, scenario)[0].click()
+
+    all_scenarios_page_elements = get_all_elements(driver, "class name", "ion-color")
+    get_elements_by_text(all_scenarios_page_elements, HERO)[0].click()
+
+    if SCENARIO != OPEN:
+        all_scenarios_hero_page_elements = get_all_elements(driver, "class name", "ion-color")
+        get_elements_by_text(all_scenarios_hero_page_elements, VILLAIN)[0].click()
+
+    all_combos = get_all_elements(driver, "class name", "ion-activatable")
+    
+    return get_flopzilla_range_string(all_combos, scenario == VS_3BET or scenario == VS_4BET)
+
+
+driver = webdriver.Chrome(executable_path='C:/Users/gilim/.wdm/drivers/chromedriver/win64/131.0.6778.264/chromedriver-win32/chromedriver.exe')
+
 
 driver.get("https://app.pokertrainer.se/rangeeditor")
 driver.maximize_window()
@@ -65,28 +101,9 @@ get_elements_by_text(all_landing_page_elements, "GTO")[0].click()
 all_landing_page_game_elements = get_all_elements(driver, "class name", "ion-color")
 get_elements_by_text(all_landing_page_game_elements, "6")[0].click()
 get_elements_by_text(all_landing_page_game_elements, "100")[0].click()
-get_elements_by_text(all_landing_page_game_elements, SCENARIO)[0].click()
 
-all_scenarios_page_elements = get_all_elements(driver, "class name", "ion-color")
-get_elements_by_text(all_scenarios_page_elements, HERO)[0].click()
+print(get_open_or_vs_raise_flopzilla_range_string(all_landing_page_game_elements, driver, SCENARIO))
 
-if VILLAIN != NAN:
-    all_scenarios_hero_page_elements = get_all_elements(driver, "class name", "ion-color")
-    if SCENARIO == VS_RAISE:
-        get_elements_by_text(all_scenarios_hero_page_elements, VILLAIN)[1].click()
-    else:
-        print("Gil you need to choose another array index")
-        get_elements_by_text(all_scenarios_hero_page_elements, VILLAIN)[0].click()
 
-all_combos = get_all_elements(driver, "class name", "ion-activatable")
-all_combos_filtered = [combo for combo in all_combos if combo.text != "BUILT-IN" and combo.text != "YOUR RANGES" and combo.text != ""] 
-
-flopzilla_range_string = ""
-for combo in all_combos_filtered:
-    percentage = get_combo_percentage(ACTION, combo.get_attribute("style"))
-    if percentage != 0:
-        flopzilla_range_string = flopzilla_range_string + f"[{percentage}]{combo.text}[/{percentage}]" + ","
-
-print(flopzilla_range_string[:-1])
 
 driver.quit()
